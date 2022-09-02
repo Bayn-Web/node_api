a57335b76c0ae235b9c26e3e39ec9fccbf29c46b
Bayn-Web <Bayn1404@gmail.com>
2022-09-02 20:22:34 +0800

    update express to koa2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require("express")
+// const express = require("express")
+// const app = express()
 const router = require("./router")
-const app = express()
 
+
+const koa = require("koa")
+const app = new koa
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 // const uri = "mongodb+srv://bayn:<password>@shortsaver.4vempyp.mongodb.net/?retryWrites=true&w=majority";
@@ -12,29 +15,39 @@ const app = express()
 //   client.close();
 // });
 
-app.all("*",function(req,res,next){
-    // 设置允许跨域的域名,*代表允许任意域名跨域
-    res.header('Access-Control-Allow-Origin','*');
-    // 允许的header类型
-    res.header('Access-Control-Allow-Headers','content-type');
-    // 跨域允许的请求方式
-    res.header('Access-Control-Allow-Methods','DELETE,PUT,POST,GET,OPTIONS');
-    if(req.method.toLowerCase() == 'options')
-        res.send(200); // 让options 尝试请求快速结束
-    else
-        next();
+// app.all("*",function(req,res,next){
+//     // 设置允许跨域的域名,*代表允许任意域名跨域
+//     res.header('Access-Control-Allow-Origin','*');
+//     // 允许的header类型
+//     res.header('Access-Control-Allow-Headers','content-type');
+//     // 跨域允许的请求方式
+//     res.header('Access-Control-Allow-Methods','DELETE,PUT,POST,GET,OPTIONS');
+//     if(req.method.toLowerCase() == 'options')
+//         res.send(200); // 让options 尝试请求快速结束
+//     else
+//         next();
+// })
+
+
+app.use(async (ctx,next)=>{
+	ctx.set('Access-Control-Allow-Origin','*')
+	ctx.set('Access-Control-Allow-Headers','Content-Type,Content-Length,Authorization,Accept,X-Requested-With')
+	ctx.set('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS')
+	if(ctx.method=='OPTIONS'){
+		ctx.body = 200;
+	}else{
+		await next()
+	}
 })
 
-app.use(router)
+app.use(router.routes())
 // app.get("/get/:id",(req,res)=>{
 //     res.send(req.params);          
 // })
 
 //还能写成('/sta',express.static("./static"))表示在uri上加入/sta
-app.use(express.static("./static"))
+// app.use(express.static("./static"))
 
-app.listen(80,()=>{
-    console.log('start');
-})
+app.listen(80)
 
 module.exports=app
--- router.js
+++ router.js
@@ -1,10 +1,28 @@
-const app = require("express")
-const router = app.Router()
+// const app = require("express")
+// const router = app.Router()
+const koa = require("koa")
+const Router = require("koa-router")
+const app = new koa()
+const router = new Router()
+// router.use(app.json())
+// router.get("/get",async (req,res)=>{
+//     req.query.url=req.query.url
+//     console.log(req.query.url);
+//     let str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// 	let result = "";
+//     let len = 4
+// 	while(len) {
+// 		let index = Math.floor(Math.random() * str.length);
+// 		result += str[index];
+// 		--len;
+// 	}
+//     //生成4位随机string[]
+//     res.send({"newurl":result});
+// })
 
-router.use(app.json())
-router.get("/get",(req,res)=>{
-    req.query.url=req.query.url
-    console.log(req.query.url);
+
+router.get("/get/:id",async (ctx)=>{
+    console.log(ctx.request.url);
     let str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	let result = "";
     let len = 4
@@ -14,20 +32,35 @@ router.get("/get",(req,res)=>{
 		--len;
 	}
     //生成4位随机string[]
-    res.send({"newurl":result});
+    ctx.body = result
+    // res.send({"newurl":result});
 })
 
-router.post("/post",(req,res)=>{
-    req.body.url=req.body.url
-    req.body.newurl=req.body.newurl
-    const body = req.body
+// router.post("/post",async (req,res)=>{
+//     req.body.url=req.body.url
+//     req.body.newurl=req.body.newurl
+//     const body = req.body
+//     //判断输入链接是否与数据库重复
+//     console.log(body);
+//     //判断输入短链是否与数据库重复
+//     const returnbody = req.body
+//     returnbody.promision = true
+//     //处理短链
+//     res.send(req.body);
+// })
+
+router.post("/post",async (ctx)=>{
+    console.log(ctx.request.body.url);  
+    // req.body.newurl=req.body.newurl
+    const body = ctx.request.body
     //判断输入链接是否与数据库重复
     console.log(body);
     //判断输入短链是否与数据库重复
-    const returnbody = req.body
-    returnbody.promision = true
+    // const returnbody = req.body
+    body.promision = true
     //处理短链
-    res.send(req.body);
+    ctx.body=body
 })
+
 
 module.exports=router
