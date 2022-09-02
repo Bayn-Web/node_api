c4cbdfd81205734326add051242baddead030932
Bayn-Web <Bayn1404@gmail.com>
2022-09-03 00:14:36 +0800

    back a express version

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// const express = require("express")
-// const app = express()
+const express = require("express")
+const app = express()
 const router = require("./router")
 
 
-const koa = require("koa")
-const app = new koa()
-const bodyParser = require("koa-bodyparser")
+// const koa = require("koa")
+// const app = new koa()
+// const bodyParser = require("koa-bodyparser")
 
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -17,39 +17,39 @@ const bodyParser = require("koa-bodyparser")
 //   client.close();
 // });
 
-// app.all("*",function(req,res,next){
-//     // 设置允许跨域的域名,*代表允许任意域名跨域
-//     res.header('Access-Control-Allow-Origin','*');
-//     // 允许的header类型
-//     res.header('Access-Control-Allow-Headers','content-type');
-//     // 跨域允许的请求方式
-//     res.header('Access-Control-Allow-Methods','DELETE,PUT,POST,GET,OPTIONS');
-//     if(req.method.toLowerCase() == 'options')
-//         res.send(200); // 让options 尝试请求快速结束
-//     else
-//         next();
-// })
-
-
-app.use(async (ctx,next)=>{
-	ctx.set('Access-Control-Allow-Origin','*')
-	ctx.set('Access-Control-Allow-Headers','Content-Type,Content-Length,Authorization,Accept,X-Requested-With')
-	ctx.set('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS')
-	if(ctx.method=='OPTIONS'){
-		ctx.body = 200;
-	}else{
-		await next()
-	}
+app.all("*",function(req,res,next){
+    // 设置允许跨域的域名,*代表允许任意域名跨域
+    res.header('Access-Control-Allow-Origin','*');
+    // 允许的header类型
+    res.header('Access-Control-Allow-Headers','content-type');
+    // 跨域允许的请求方式
+    res.header('Access-Control-Allow-Methods','DELETE,PUT,POST,GET,OPTIONS');
+    if(req.method.toLowerCase() == 'options')
+        res.send(200); // 让options 尝试请求快速结束
+    else
+        next();
 })
-app.use(bodyParser())
+
+
+// app.use(async (ctx,next)=>{
+// 	ctx.set('Access-Control-Allow-Origin','*')
+// 	ctx.set('Access-Control-Allow-Headers','Content-Type,Content-Length,Authorization,Accept,X-Requested-With')
+// 	ctx.set('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS')
+// 	if(ctx.method=='OPTIONS'){
+// 		ctx.body = 200;
+// 	}else{
+// 		await next()
+// 	}
+// })
+// app.use(bodyParser())
 //使用body-parser插件 否则无法获取post的request.body
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router)
 // app.get("/get/:id",(req,res)=>{
 //     res.send(req.params);          
 // })
 
 //还能写成('/sta',express.static("./static"))表示在uri上加入/sta
-// app.use(express.static("./static"))
+app.use(express.static("./static"))
 
 app.listen(80)
 
--- router.js
+++ router.js
@@ -1,30 +1,13 @@
-// const app = require("express")
-// const router = app.Router()
-const koa = require("koa")
-const Router = require("koa-router")
-const app = new koa()
-const router = new Router()
-// router.use(app.json())
-// router.get("/get",async (req,res)=>{
-//     req.query.url=req.query.url
-//     console.log(req.query.url);
-//     let str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// 	let result = "";
-//     let len = 4
-// 	while(len) {
-// 		let index = Math.floor(Math.random() * str.length);
-// 		result += str[index];
-// 		--len;
-// 	}
-//     //生成4位随机string[]
-//     res.send({"newurl":result});
-// })
-
-
-router.get("/get",async (ctx)=>{
-    const {url} = ctx.query
-    console.log(url);
-    console.log(ctx.request.body);
+const app = require("express")
+const router = app.Router()
+// const koa = require("koa")
+// const Router = require("koa-router")
+// const app = new koa()
+// const router = new Router()
+router.use(app.json())
+router.get("/get",async (req,res)=>{
+    req.query.url=req.query.url
+    console.log(req.query.url);
     let str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	let result = "";
     let len = 4
@@ -34,35 +17,50 @@ router.get("/get",async (ctx)=>{
 		--len;
 	}
     //生成4位随机string[]
-    ctx.body = {newurl : result}
-    // res.send({"newurl":result});
+    res.send({"newurl":result});
 })
 
-// router.post("/post",async (req,res)=>{
-//     req.body.url=req.body.url
-//     req.body.newurl=req.body.newurl
-//     const body = req.body
+
+// router.get("/get",async (ctx)=>{
+//     const {url} = ctx.query
+//     console.log(url);
+//     console.log(ctx.request.body);
+//     let str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// 	let result = "";
+//     let len = 4
+// 	while(len) {
+// 		let index = Math.floor(Math.random() * str.length);
+// 		result += str[index];
+// 		--len;
+// 	}
+//     //生成4位随机string[]
+//     ctx.body = {newurl : result}
+//     // res.send({"newurl":result});
+// })
+
+router.post("/post",async (req,res)=>{
+    // req.body.url=req.body.url
+    //判断输入链接是否与数据库重复
+    //判断输入短链是否与数据库重复
+    const returnbody = req.body
+    console.log(returnbody);
+    returnbody.promision = true
+    //处理短链
+    res.send(returnbody);
+})
+
+// router.post("/post",async (ctx)=>{
+//     console.log(ctx.url);  
+//     // req.body.newurl=req.body.newurl
+//     const body = ctx.request.body
 //     //判断输入链接是否与数据库重复
 //     console.log(body);
 //     //判断输入短链是否与数据库重复
-//     const returnbody = req.body
-//     returnbody.promision = true
+//     // const returnbody = req.body
+//     body.promision = true
 //     //处理短链
-//     res.send(req.body);
+//     ctx.body=body
 // })
-
-router.post("/post",async (ctx)=>{
-    console.log(ctx.url);  
-    // req.body.newurl=req.body.newurl
-    const body = ctx.request.body
-    //判断输入链接是否与数据库重复
-    console.log(body);
-    //判断输入短链是否与数据库重复
-    // const returnbody = req.body
-    body.promision = true
-    //处理短链
-    ctx.body=body
-})
 
 
 module.exports=router
