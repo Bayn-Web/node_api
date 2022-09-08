68353736a6cb04cfceb08a15c6d73ed90627f773
Bayn-Web <Bayn1404@gmail.com>
2022-09-08 23:10:13 +0800

    fix post

--- router.js
+++ router.js
@@ -34,16 +34,16 @@ router.post("/post",async (ctx)=>{
     console.log(body);
     await db.findByUrl(ctx.request.body.url, ctx.request.body.newurl).then(res=>{
         if (res.length == 0){
-            await db.save(ctx.request.body.url, ctx.request.body.newurl).then(res=>{
+            db.save(ctx.request.body.url, ctx.request.body.newurl).then(res=>{
                 console.log("add suc!")
                 body.promision = true
             })
-        }else{
-            console.log("the url is repeat")
-            result = "url repeated"
-            body.promision = false
         }
     })
+    if(body.promision != true){
+        body.promision = false
+        result = "url repeated"
+    }
     //处理短链
     ctx.body=body
 })
